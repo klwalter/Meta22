032918ea48c4428c092080c0522599d20eab97dc
klwalter <kamil.walter4@gmail.com>
2022-05-06 16:46:50 +0200

    more plots

--- Projekt2/plots2.py
+++ Projekt2/plots2.py
@@ -18,7 +18,7 @@ for line in lista:
     while line[i] != " ": 
         i += 1
     x = int(line[:i])
-    y = float(line[i+1:-1])
+    y = float(line[i+1:-1])/x
     arguments.append(x)
     values.append(y)
 
@@ -38,34 +38,34 @@ for line in lista:
     while line[i] != " ": 
         i += 1
     x = int(line[:i])
-    y = float(line[i+1:-1])
+    y = float(line[i+1:-1])/x
     arguments.append(x)
     values.append(y)
 
 plt.plot(arguments, values, label='extended neighbour')
 
 
-file = open("data/3/time_comp.txt", "r")
-lista = []
-for line in file:
-    lista.append(line)
-file.close()
-arguments = []
-values = []
-for line in lista:
-    n = len(line)
-    i = 1
-    while line[i] != " ": 
-        i += 1
-    x = int(line[:i])
-    y = 10**8*float(line[i+1:-1])/(x**4)
-    arguments.append(x)
-    values.append(y)
+# file = open("data/3/time_comp.txt", "r")
+# lista = []
+# for line in file:
+#     lista.append(line)
+# file.close()
+# arguments = []
+# values = []
+# for line in lista:
+#     n = len(line)
+#     i = 1
+#     while line[i] != " ": 
+#         i += 1
+#     x = int(line[:i])
+#     y = 10**9*float(line[i+1:-1])/(x**4)
+#     arguments.append(x)
+#     values.append(y)
 
-plt.plot(arguments, values, label='2opt')
+# plt.plot(arguments, values, label='2opt')
 
 plt.legend()
-plt.show()
+
 
 # z = np.polyfit(arguments,values,15)
 # p = np.poly1d(z)
@@ -75,5 +75,5 @@ plt.show()
 # x = np.average(values)
 # arr = [x] * len(values)
 # plt.plot(arguments,arr)
-#plt.ylim(-0.05,2)
-plt.savefig("plots/3/divided_time_berlin.png")
+plt.ylim(-0.05, 1)
+plt.savefig("plots/3/divided_time_rest.png")
